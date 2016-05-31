8c4617b3d1557fb3a52189f0b4f48cf0dd67fe2d
Memphis796 <memphis796@Hotmail.com>
2016-05-31 19:12:12 -0400

    finished login-form

--- src/login-form.js
+++ src/login-form.js
@@ -1,2 +1,36 @@
 
-export default undefined;
+export default class LoginForm {
+  constructor(form, email) {
+    this.form = form;
+    this.validCredentials = [
+      { user: `aaron@theironyard.com`, password: `password123` },
+      { user: `admin@google.com`, password: `pandas` },
+      { user: email, password: `honeycrisp` },
+    ];
+
+    this.form.addEventListener(`submit`, (ev) => {
+      ev.preventDefault();
+      this.validateInputs();
+    });
+  }
+
+  validate(username, password) {
+    return this.validCredentials.reduce((guest, guestUser) => {
+      if (guestUser.user === username && guestUser.password === password) {
+        return true;
+      }
+      return guest;
+    }, false);
+  }
+  validateInputs() {
+    const emailInput = this.form.querySelector(`.login-form__email`).value;
+    const passwordInput = this.form.querySelector(`.login-form__password`).value;
+    const validationMessage = this.form.querySelector(`.login-form__validation-message`);
+
+    if (this.validate(emailInput, passwordInput)) {
+      validationMessage.innerHTML = ``;
+    } else {
+      validationMessage.innerHTML = `The credentials are invalid`;
+    }
+  }
+}
